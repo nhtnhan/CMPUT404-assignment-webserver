636eda6305ab49b42141d376afc10099c9b4b660
Nhan <nhnguyen@ualberta.ca>
2021-01-28 19:26:33 -0700

    edited server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import os
+import mimetypes
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,13 +28,54 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+NEWLINE = "\r\n"
+
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        
+        # PROCESS DATA TO HEADERS
+        # retrieve method, path, protocol from the first line
+        method,path,protocol = [part.strip() for part in self.data.splitlines()[0].decode().split()]
+        if (method =="GET"):
+            self.processRequest(path)
+        else:
+            # 405
+            code = 'HTTP/1.1 405 Method not allowed' + NEWLINE
+            self.request.sendall(bytearray(code,'utf-8'))
+            self.request.sendall(bytearray("Connection: closed"+NEWLINE+NEWLINE,'utf-8'))
+    
+    def processRequest(self,path):
+        ppath = "www"+path
+        # header = {}
+
+        # check unsecured path:
+        if (".." not in ppath):
+            if (os.path.isdir(ppath) or os.path.isfile(ppath)):
+                # print("200 OK")
+                if ("." not in ppath):
+                    filetype ="html"
+                else:
+                    filetype = ppath.split(".")[-1]
+                #PROCESS APPROPRIATE HEADER AND CONTENT
+                code = 'HTTP/1.1 200 OK' + NEWLINE
+                content_type = 'Content-Type: text/'+filetype + NEWLINE
+                self.request.sendall(bytearray(code,'utf-8'))
+                self.request.sendall(bytearray(content_type,'utf-8'))
+                self.request.sendall(bytearray("Connection: closed"+NEWLINE+NEWLINE,'utf-8'))
+            else:
+                code = 'HTTP/1.1 404 Not found' + NEWLINE
+                self.request.sendall(bytearray(code,'utf-8'))
+                self.request.sendall(bytearray("Connection: closed"+NEWLINE+NEWLINE,'utf-8'))
+        else:
+            code = 'HTTP/1.1 404 Not Found' + NEWLINE
+            self.request.sendall(bytearray(code,'utf-8'))
+            self.request.sendall(bytearray("Connection: closed"+NEWLINE+NEWLINE,'utf-8'))
+
+        # self.request.sendall(bytearray("OK",'utf-8'))
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
